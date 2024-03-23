5747471a79246173bc02813843f47d680a000fd7
BUNSAK12 <163999880+BUNSAK12@users.noreply.github.com>
2024-03-23 15:22:31 +0700

    Add files via upload

---
+++ Testform.php
@@ -0,0 +1,74 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <form action="html.php" method="post">
+    <div class="card">
+  <div class="card-header​ bg-success">គណនាត្រីកោណសម័ញ្ញ</div>
+  <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                    <label for="" class="fw-bold">Math</label>
+                    <input type="text" class="form-control mb-3 focus-ring " name="age1" id="" require>
+                    </div>
+
+                    <div class="col-md-6">
+                    <label for="" class="fw-bold">Khmer</label>
+
+                    <input type="text" class="form-control mb-3 focus-ring " name="age2" id="" require>
+                    </div>
+
+
+
+
+                </div>
+
+
+                <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+            
+           
+         
+
+   
+  </div>
+  <div class="card-footer">
+  <div class="d-grid gap-2 col-6 mx-auto">
+  <button type="sumit" class="btn btn-outline-primary" type="button">គណនា</button>
+</div>
+  </div>
+  </form>
+</div>
+    </div>
+    
+</body>
+</html>
